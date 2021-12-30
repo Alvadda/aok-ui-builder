2f339e4551d63d9110c4ee9475c0b3338f9124b7
Alvadda <58438582+Alvadda@users.noreply.github.com>
2021-12-30 20:29:39 +0100

    styling the controll bar

--- public/aokub.js
+++ public/aokub.js
@@ -1,17 +1,18 @@
 ;(function () {
   const MODIFIER = 1
-  const TO_FULL_HD = 1.4286
   const GAME_HEIGHT = 1080
   const GAME_WIDTH = 1920
 
   const frames = document.querySelectorAll('.frame')
   const outputAlly = document.querySelector('.output-ally')
   const outputEnemy = document.querySelector('.output-emeny')
-  const copyAlly = document.querySelector('.copy-ally')
-  const copyEnemy = document.querySelector('.copy-enemy')
   const ui = document.querySelector('.ui')
+  const controllContainer = document.querySelector('.controll')
+
   const switchGrid = document.querySelector('#grid-switch')
 
+  const buttonCopyAlly = document.querySelector('.copy-ally')
+  const buttonCopyEnemy = document.querySelector('.copy-enemy')
   const buttonDefaultPreset = document.querySelector('#set-default-preset')
   const buttonFarPreset = document.querySelector('#set-far-preset')
   const buttonClosePreset = document.querySelector('#set-close-preset')
@@ -72,7 +73,7 @@
       const frameKeyX = `${frame.id}_X_OFFSET`
       const frameKeyY = `${frame.id}_Y_OFFSET`
       const frameValueX = calcOffset(x, ui.offsetWidth, GAME_WIDTH)
-      const frameValueY = calcOffset(y, ui.offsetWidth, GAME_WIDTH)
+      const frameValueY = calcOffset(y, ui.offsetHeight, GAME_HEIGHT)
 
       if (frame.id.includes('ALLY')) {
         allyPositions.push(`"${frame.id}_HIDDEN": "false",`)
@@ -162,11 +163,11 @@
     setPreset(getLastPreset())
   }
 
-  copyAlly.onclick = () => {
+  buttonCopyAlly.onclick = () => {
     addToClipboard(outputAlly.value)
   }
 
-  copyEnemy.onclick = () => {
+  buttonCopyEnemy.onclick = () => {
     addToClipboard(outputEnemy.value)
   }
 
